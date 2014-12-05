8b1bee9d017171b25f5d9f15a858b3830b7218d7
smhhy <mhoseinhoseini@gmail.com>
2014-12-05 19:09:11 +0330

    push correct version

---
+++ SecondPhase/hebo/protected/views/site/pages/columns.php
@@ -0,0 +1,122 @@
+    	<div class="page-header">
+    		<h1>Grid columns<small> responsive columns</small></h1>
+        </div>
+        
+        <div class="row-fluid">
+           <div class="span12">
+           		<h3 class="header">Full width column
+                    <span class="header-line"></span> 
+                </h3>
+           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque malesuada aliquam vulputate. Cras pulvinar erat ac velit eleifend porttitor at eu diam. Praesent elit mi, mattis vitae accumsan bibendum, porttitor non neque. Phasellus dignissim sagittis felis vel rutrum. Sed eget mi in massa luctus aliquam. In tincidunt scelerisque turpis placerat interdum. Donec convallis diam dolor, eu mollis magna. Aliquam a ante risus. Aliquam dapibus, lacus eu tristique congue, eros enim tempor nisl, at tempus enim erat nec leo. Ut ac tellus purus, eget pulvinar mauris. Maecenas tincidunt lectus at purus adipiscing id dapibus lectus vestibulum. Praesent ac eros vel tellus pulvinar porttitor at non ante.
+           </div>
+          
+        </div><!--/row-fluid-->
+        
+        
+        
+        <div class="row-fluid">
+           <div class="span6">
+           		<h3 class="header">1/2 column
+                    <span class="header-line"></span> 
+                </h3>
+           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque malesuada aliquam vulputate. Cras pulvinar erat ac velit eleifend porttitor at eu diam. Praesent elit mi, mattis vitae accumsan bibendum, porttitor non neque. Phasellus dignissim sagittis felis vel rutrum. Sed eget mi in massa luctus aliquam. In tincidunt scelerisque turpis placerat interdum.
+           </div>
+           <div class="span6">
+           		<h3 class="header">1/2 column
+                    <span class="header-line"></span> 
+                </h3>
+           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque malesuada aliquam vulputate. Cras pulvinar erat ac velit eleifend porttitor at eu diam. Praesent elit mi, mattis vitae accumsan bibendum, porttitor non neque. Phasellus dignissim sagittis felis vel rutrum. Sed eget mi in massa luctus aliquam. In tincidunt scelerisque turpis placerat interdum.
+           </div>
+          
+        </div><!--/row-fluid-->
+        
+        <div class="row-fluid">
+           <div class="span4">
+           		<h3 class="header">1/3 column
+                    <span class="header-line"></span> 
+                </h3>
+           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque malesuada aliquam vulputate. Cras pulvinar erat ac velit eleifend porttitor at eu diam. Praesent elit mi, mattis vitae accumsan bibendum, porttitor non neque. 
+           </div>
+           <div class="span4">
+           		<h3 class="header">1/3 column
+                    <span class="header-line"></span> 
+                </h3>
+           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque malesuada aliquam vulputate. Cras pulvinar erat ac velit eleifend porttitor at eu diam. Praesent elit mi, mattis vitae accumsan bibendum, porttitor non neque. 
+           </div>
+           <div class="span4">
+           		<h3 class="header">1/3 column
+                    <span class="header-line"></span> 
+                </h3>
+           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque malesuada aliquam vulputate. Cras pulvinar erat ac velit eleifend porttitor at eu diam. Praesent elit mi, mattis vitae accumsan bibendum, porttitor non neque. 
+           </div>
+          
+        </div><!--/row-fluid-->
+        
+        <div class="row-fluid">
+           <div class="span3">
+           		<h3 class="header">1/4 column
+                    <span class="header-line"></span> 
+                </h3>
+           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque malesuada aliquam vulputate. Cras pulvinar erat ac velit eleifend porttitor at eu diam. Praesent elit mi, mattis vitae accumsan bibendum, porttitor non neque. 
+           </div>
+           <div class="span3">
+           		<h3 class="header">1/4 column
+                    <span class="header-line"></span> 
+                </h3>
+           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque malesuada aliquam vulputate. Cras pulvinar erat ac velit eleifend porttitor at eu diam. Praesent elit mi, mattis vitae accumsan bibendum, porttitor non neque. 
+           </div>
+           <div class="span3">
+           		<h3 class="header">1/4 column
+                    <span class="header-line"></span> 
+                </h3>
+           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque malesuada aliquam vulputate. Cras pulvinar erat ac velit eleifend porttitor at eu diam. Praesent elit mi, mattis vitae accumsan bibendum, porttitor non neque. 
+           </div>
+           <div class="span3">
+           		<h3 class="header">1/4 column
+                    <span class="header-line"></span> 
+                </h3>
+           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque malesuada aliquam vulputate. Cras pulvinar erat ac velit eleifend porttitor at eu diam. Praesent elit mi, mattis vitae accumsan bibendum, porttitor non neque. 
+           </div>
+          
+        </div><!--/row-fluid-->
+        
+        
+        <div class="row-fluid">
+           <div class="span2">
+           		<h3 class="header">1/6 column
+                    <span class="header-line"></span> 
+                </h3>
+           Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+           </div>
+           <div class="span2">
+           		<h3 class="header">1/6 column
+                    <span class="header-line"></span> 
+                </h3>
+           Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+           </div>
+           <div class="span2">
+           		<h3 class="header">1/6 column
+                    <span class="header-line"></span> 
+                </h3>
+           Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+           </div>
+           <div class="span2">
+           		<h3 class="header">1/6 column
+                    <span class="header-line"></span> 
+                </h3>
+           Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+           </div>
+           <div class="span2">
+           		<h3 class="header">1/6 column
+                    <span class="header-line"></span> 
+                </h3>
+           Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+           </div>
+           <div class="span2">
+           		<h3 class="header">1/6 column
+                    <span class="header-line"></span> 
+                </h3>
+           Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+           </div>
+          
+        </div><!--/row-fluid-->
---
+++ SecondPhase/hebo/protected/views/site/pages/register.php
@@ -0,0 +1,63 @@
+<div class="container shout-box">
+    <div class="shout-text">
+      <h1>ثبت نام</h1>
+      <p>لطفا فرم زیر را به دقت پر کنید.</p>
+    </div>
+</div>
+
+        <div class="row-fluid"style="direction: rtl">
+
+            <div class="span6">
+
+                <div class="row-fluid">
+                    <label>وضعیت تاهل</label>
+                    <div class="row-fluid">
+                        <div class="span6">
+                            <input class="span6" name="Taahhol" type="radio"  value="0">مجرد
+                        </div>
+                        <div class="span6">
+                            <input class="span6" name="Taahhol" type="radio" value="1">متاهل
+                        </div>
+                    </div>
+                    <label>جنسیت</label>
+                    <div class="row-fluid">
+                        <div class="span6">
+                            <input class="span6" name="Sex" type="radio"  value="0">مرد
+                        </div>
+                        <div class="span6">
+                            <input class="span6" name="Sex" type="radio" value="1">زن
+                        </div>
+                    </div>
+                    <label>شماره موبایل</label>
+                    <input class="span12" name="Mobile" type="tel" placeholder="09xxxxxxxxx">
+                    <label>تلفن منزل</label>
+                    <input class="span12" name="Tel" type="tel" placeholder="021xxxxxxxx">
+                    <label>شهر</label>
+                    <input class="span12" name="City" type="text" placeholder="شهر">
+                    <label>آدرس</label>
+                    <textarea rows="2" class="span12" name="Address" type="text" placeholder="آدرس"></textarea>
+                    <p>
+                        <button class="btn" type="button">ثبت</button>
+                    </p>
+
+                </div>
+                </div>
+                <div class="span6">
+                    <div class="row-fluid">
+                        <label>نام</label>
+                        <input class="span12" name="FirstName" type="text" placeholder="نام">
+                        <label>نام خانوادگی</label>
+                        <input class="span12" name="LastName" type="text" placeholder="نام خانوادگی">
+                        <label>نام پدر</label>
+                        <input class="span12" name="FatherName" type="text" placeholder="نام پدر">
+                        <label>کد ملی</label>
+                        <input class="span12" name="NationCode" type="text" placeholder="کد ملی">
+                        <label>شماره شناسنامه</label>
+                        <input class="span12" name="ShCode" type="text" placeholder="شماره شناسنامه">
+                        <label>آدرس ایمیل</label>
+                        <input class="span12" name="EmailAddress" type="email" placeholder="example@company.com">
+
+
+                    </div>
+            </div>
+    </div>
