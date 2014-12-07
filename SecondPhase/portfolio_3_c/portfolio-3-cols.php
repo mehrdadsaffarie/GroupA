351eec576213c5a0d45412341da547d06bf23aab
Javad-Forough <javadforough@gmail.com>
2014-12-07 22:19:53 +0330

    added portfolio 3 Column to phase 2

---
+++ SecondPhase/portfolio_3_c/portfolio-3-cols.php
@@ -0,0 +1,173 @@
+		<?php
+			$baseUrl = Yii::app()->request->baseUrl;
+		?>
+    	<div class="page-header">
+    		<h1>Portfolio <small> some of our work</small></h1>
+        </div>
+        <div class="row-fluid">
+         
+                <h3 class="header">3 columns
+                    <span class="header-line"></span> 
+                </h3>
+                
+                <ul class="portfolio thumbnails ">
+                  
+                  
+                  <li class="span4">
+                    <a href="img/portfolio/shohada/40-Mohammad Saeed Yazdanparast.jpg" rel="lightbox"  class="thumbnail">
+                    <div class="background"><img src="<?php echo $baseUrl;?>/img/portfolio/shohada/40-Mohammad Saeed Yazdanparast.jpg" alt="" class="img-polaroid"></div>
+                    </a>
+                  </li>
+                  
+                  <li class="span4">
+                    <a href="img/portfolio/shohada/50-Mohammadreza Taheri.jpg" rel="lightbox" class="thumbnail"><img src="<?php echo $baseUrl;?>/img/portfolio/shohada/50-Mohammadreza Taheri.jpg" alt="" class="img-polaroid"></a>
+                  </li>
+                  
+                  <li class="span4">
+                    <a href="img/portfolio/shohada/81-Bagher Cheraghi.jpg" rel="lightbox" class="thumbnail"><img src="<?php echo $baseUrl;?>/img/portfolio/shohada/81-Bagher Cheraghi.jpg" alt="" class="img-polaroid"></a>
+                  </li>
+                  
+                  <li class="span4">
+                    <a href="img/portfolio/shohada/82-Aliasghar Zooji.jpg" rel="lightbox" class="thumbnail">
+                    <div class="background"><img src="<?php echo $baseUrl;?>/img/portfolio/shohada/82-Aliasghar Zooji.jpg" alt="" class="img-polaroid"></div>
+                    </a>
+                  </li>
+                  
+                  <li class="span4">
+                    <a href="img/portfolio/shohada/83-Baghdadi.jpg" rel="lightbox" class="thumbnail"><img src="<?php echo $baseUrl;?>/img/portfolio/shohada/83-Baghdadi.jpg" alt="" class="img-polaroid"></a>
+                  </li>
+                  
+                  <li class="span4">
+                    <a href="img/portfolio/shohada/84-Karamali Hasan Rezaii.jpg" rel="lightbox" class="thumbnail"><img src="<?php echo $baseUrl;?>/img/portfolio/shohada/84-Karamali Hasan Rezaii.jpg" alt="" class="img-polaroid"></a>
+                  </li>
+                  
+                  <li class="span4">
+                    <a href="img/portfolio/shohada/85-Kamyar Hajhosseinali TAvafi.jpg" rel="lightbox" class="thumbnail">
+                    <div class="background"><img src="<?php echo $baseUrl;?>/img/portfolio/shohada/85-Kamyar Hajhosseinali TAvafi.jpg" alt="" class="img-polaroid"></div>
+                    </a>
+                  </li>
+                  
+                  <li class="span4">
+                    <a href="img/portfolio/shohada/86-Mohammad Sarfi.jpg" rel="lightbox" class="thumbnail"><img src="<?php echo $baseUrl;?>/img/portfolio/shohada/86-Mohammad Sarfi.jpg" alt="" class="img-polaroid"></a>
+                  </li>
+                  
+                  <li class="span4">
+                    <a href="img/portfolio/shohada/88-Majid Vasefi.jpg" rel="lightbox" class="thumbnail"><img src="<?php echo $baseUrl;?>/img/portfolio/shohada/88-Majid Vasefi.jpg" alt="" class="img-polaroid"></a>
+                  </li>
+    			
+                
+                
+ 
+  				  <li class="span4">
+                    <a href="img/portfolio/shohada/89-Morteza Vaezi.jpg" rel="lightbox" class="thumbnail"><img src="<?php echo $baseUrl;?>/img/portfolio/shohada/89-Morteza Vaezi.jpg" alt="" class="img-polaroid"></a>
+                  </li>
+    			
+                
+                
+  				  <li class="span4">
+                    <a href="img/portfolio/shohada/90-Mohammad Taghi Rezaeii.jpg" rel="lightbox" class="thumbnail"><img src="<?php echo $baseUrl;?>/img/portfolio/shohada/90-Mohammad Taghi Rezaeii.jpg" alt="" class="img-polaroid"></a>
+                  </li>
+    			
+                
+                
+                  <li class="span4">
+                    <a href="/protected/views/site/pages/p_3_c_vasiat/91_shahid_hamidreza_shekarchi.php" rel="lightbox" class="thumbnail"><img src="<?php echo $baseUrl;?>/img/portfolio/shohada/91-Hamidreza Shekarchi.jpg" alt="" class="img-polaroid"></a>
+                  </li>
+    			
+                
+                <li class="span4">
+                    <a href="img/portfolio/shohada/92-Alireza Ameri Toorzani.jpg" rel="lightbox" class="thumbnail"><img src="<?php echo $baseUrl;?>/img/portfolio/shohada/92-Alireza Ameri Toorzani.jpg" alt="" class="img-polaroid"></a>
+                  </li>
+                
+                
+                
+                <li class="span4">
+                    <a href="img/portfolio/shohada/93-Mohammad Fathali Shahi.jpg" rel="lightbox" class="thumbnail"><img src="<?php echo $baseUrl;?>/img/portfolio/shohada/93-Mohammad Fathali Shahi.jpg" alt="" class="img-polaroid"></a>
+                  </li>
+                
+                
+                
+                <li class="span4">
+                    <a href="img/portfolio/shohada/94-Gholamreza Saberi.jpg" rel="lightbox" class="thumbnail"><img src="<?php echo $baseUrl;?>/img/portfolio/shohada/94-Gholamreza Saberi.jpg" alt="" class="img-polaroid"></a>
+                  </li>
+                
+                
+                
+                
+                
+                <li class="span4">
+                    <a href="img/portfolio/shohada/96-Iraj Soorkiazad.jpg" rel="lightbox" class="thumbnail"><img src="<?php echo $baseUrl;?>/img/portfolio/shohada/96-Iraj Soorkiazad.jpg" alt="" class="img-polaroid"></a>
+                  </li>
+                
+                
+                
+                
+                
+                
+                <li class="span4">
+                    <a href="img/portfolio/shohada/98-Mohamma d Kazem Zareii.jpg" rel="lightbox" class="thumbnail"><img src="<?php echo $baseUrl;?>/img/portfolio/shohada/98-Mohamma d Kazem Zareii.jpg" alt="" class="img-polaroid"></a>
+                  </li>
+                
+                
+                
+                
+                <li class="span4">
+                    <a href="img/portfolio/shohada/99-Mohammadreza Seifi.jpg" rel="lightbox" class="thumbnail"><img src="<?php echo $baseUrl;?>/img/portfolio/shohada/99-Mohammadreza Seifi.jpg" alt="" class="img-polaroid"></a>
+                  </li>
+                
+                
+                
+                
+                
+                
+                
+                
+                <li class="span4">
+                    <a href="img/portfolio/shohada/100-Hamid Esmaiili Savadkoohi.jpg" rel="lightbox" class="thumbnail"><img src="<?php echo $baseUrl;?>/img/portfolio/shohada/100-Hamid Esmaiili Savadkoohi.jpg" alt="" class="img-polaroid"></a>
+                  </li>
+                
+                
+                
+                
+                
+                
+                
+                <li class="span4">
+                    <a href="img/portfolio/shohada/102-Mohammadreza Tabaiizade.jpg" rel="lightbox" class="thumbnail"><img src="<?php echo $baseUrl;?>/img/portfolio/shohada/102-Mohammadreza Tabaiizade.jpg" alt="" class="img-polaroid"></a>
+                  </li>
+                  
+                  
+                  
+                <li class="span4">
+                    <a href="img/portfolio/shohada/103-Seyed Abolhasan Mosavi Khorshidi.jpg" rel="lightbox" class="thumbnail"><img src="<?php echo $baseUrl;?>/img/portfolio/shohada/103-Seyed Abolhasan Mosavi Khorshidi.jpg" alt="" class="img-polaroid"></a>
+                  </li>  
+                  
+                  
+                  
+                  
+                <li class="span4">
+                    <a href="img/portfolio/shohada/103-Seyed Abolhasan Mosavi Khorshidi.jpg" rel="lightbox" class="thumbnail"><img src="<?php echo $baseUrl;?>/img/portfolio/shohada/103-Seyed Abolhasan Mosavi Khorshidi.jpg" alt="" class="img-polaroid"></a>
+                  </li>  
+                  
+                  
+                  
+                  
+                  <li class="span4">
+                    <a href="protected/views/site/pages/p_3_c_vasiat/106_shahid_majid_sadeghi_nejhad.php" rel="lightbox" class="thumbnail"><img src="<?php echo $baseUrl;?>/img/portfolio/shohada/106-Majid Sadeghi nejad Saryazdi.jpg" alt="" class="img-polaroid"></a>
+                  </li>
+                  
+                  
+                  
+                  <li class="span4">
+                    <a href="img/portfolio/shohada/107-Norollah Soori.jpg" rel="lightbox" class="thumbnail"><img src="<?php echo $baseUrl;?>/img/portfolio/shohada/107-Norollah Soori.jpg" alt="" class="img-polaroid"></a>
+                  </li>
+                  
+                  
+                 
+                
+                
+                
+                </ul>
+                
+    			
+		</div><!--/row-fluid-->
