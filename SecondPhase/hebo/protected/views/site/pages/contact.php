2dee6d0a0b78e74c11af847c3f17eca457b71e7d
mohammadmirmoosavi <mirmoosavi@comp.iust.ac.ir>
2015-01-23 00:54:56 +0330

    maps google

--- SecondPhase/hebo/protected/views/site/pages/contact.php
+++ SecondPhase/hebo/protected/views/site/pages/contact.php
@@ -1,7 +1,7 @@
 <div class="container shout-box">
     <div class="shout-text">
-      <h1>Greatest website ever</h1>
-      <p>We work very hard to bring you the best website themes.<br> All our themes are responsive and look great on any device.</p>
+      <h1></h1>
+      <p> </p>
     </div>
 </div>
       
@@ -10,30 +10,31 @@
             
             	<div class="row-fluid">
                     <div class="span6">
-                        <h3>Address</h3>
+                        <h3>آدرس</h3>
                         <p>
-                        20 Glenville Road<br />
-                        Hatfeild<br />
-                        Harare<br />
-                        ZIMBABWE
+			تهران<br />
+                       خیابان هنگام <br />
+                        دانشگاه علم و صنعت ایران<br />
+                        
+                        
                         </p>
                     </div>
                     <div class="span6">
-                        <h3>Contact details</h3>
+                        <h3>جزییات مخاطب</h3>
                         <p>
                         P: +123 (456) 7890<br />
                         F: +123 (456) 7890<br />
-                        E: info@example.com<br />
-                        W: www.example.com
+                        E: mohammadmirmoosavi@gmail.com<br />
+                        W: http://immortal-yiiframework.rhcloud.com/ 
                         </p>
                     </div>
                 </div>
                 <br />
-                <label>Full Name</label>
+                <label>نام و نام خانوادگی</label>
                 <input class="span12" type="text" placeholder="Your full name">
-                <label>Email Address</label>
+                <label>ایمیل</label>
                 <input class="span12" type="text" placeholder="Your email address">
-                <label>Message</label>
+                <label>پیام</label>
                 <textarea rows="7" class="span12"  placeholder="Your message"></textarea>
                 <p>
                   <button class="btn" type="button">Send</button>
@@ -41,10 +42,29 @@
                 
             </div>
             <div class="span6">
-               
-               <!-- start google map -->
+              	
+			<script src="http://maps.googleapis.com/maps/api/js"></script>
+			<script>
+				function initialize()
+				{
+					var mapProp = {
+						center:new google.maps.LatLng(35.7403607,51.5126038),
+						zoom:10,
+						mapTypeId:google.maps.MapTypeId.ROADMAP
+				};
+				var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+				}
+				google.maps.event.addDomListener(window, 'load', initialize);
+			</script>
+				
+					
+		
+		
+			<div id="googleMap" style="width: 450px;height:380px;"></div>
+		 
+               <!-- start google map 
                	<iframe width="450" height="450" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=harare&amp;aq=&amp;sll=37.0625,-95.677068&amp;sspn=40.052282,86.572266&amp;ie=UTF8&amp;hq=&amp;hnear=Harare,+Zimbabwe&amp;t=m&amp;ll=-17.822814,31.054916&amp;spn=0.03677,0.038538&amp;z=14&amp;output=embed"></iframe><br /><small><a href="https://maps.google.com/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=harare&amp;aq=&amp;sll=37.0625,-95.677068&amp;sspn=40.052282,86.572266&amp;ie=UTF8&amp;hq=&amp;hnear=Harare,+Zimbabwe&amp;t=m&amp;ll=-17.822814,31.054916&amp;spn=0.03677,0.038538&amp;z=14" style="color:#0000FF;text-align:left">View Larger Map</a></small>
-               <!-- end google map -->
+                end google map -->
             
                 
             </div>
